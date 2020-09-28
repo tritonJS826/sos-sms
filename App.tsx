f2b8b3ff953f9484e3fa50b978629e2fcd1ee2a1
tritonJS826 <triton.68@mail.ru>
2020-09-28 17:32:03 +0300

    feat: eject

--- App.tsx
+++ App.tsx
@@ -1,46 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import VolumeControl, { VolumeControlEvents } from "react-native-volume-control";
-// это по идее надо подключить так: https://www.npmjs.com/package/react-native-volume-control
-// тогда все заработает
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import SystemSetting from "react-native-system-setting";
 
-import { header, howToUse } from './src/constants/mainPageContent';
+import { header, howToUse } from "./src/constants/mainPageContent";
 
 const App = () => {
   const [volume, setVolume] = useState(0);
 
-
-  const volEvent = () => VolumeControlEvents.addListener(
-      "VolumeChanged",
-      volumeEvent,
-    );
-
-  const volumeEvent = (event: any) => {
-    setVolume(event.volume);
-  };
-
   useEffect(() => {
-    volEvent();
-    console.log(VolumeControl); // тут должен быть не null
-    // return volEvent.remove();
-  }, []);
+    const volumeListener = SystemSetting.addVolumeListener((/* data */) => {
+      setVolume(volume + 1);
+    });
+
+    return () => {
+      SystemSetting.removeVolumeListener(volumeListener);
+    };
+  }, [volume]);
 
   return (
     <View style={styles.container}>
       <Text>{header}</Text>
       <Text>volume: {volume}</Text>
-      <Button title="help" onPress={() => { alert(howToUse) }} />
+      <Button
+        title="help"
+        onPress={() => {
+          alert(howToUse);
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default App;
